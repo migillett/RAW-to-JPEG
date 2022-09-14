46a112ab71670b8f41a4c1445f6e1ec399591b6e
Michael Gillett <51103663+migillett@users.noreply.github.com>
2022-09-14 10:56:36 -0400

    added multiprocessing

--- raw_to_jpeg.py
+++ raw_to_jpeg.py
@@ -1,45 +1,71 @@
 import rawpy
 import imageio
-import os.path
+from os import path, walk, remove
+from sys import exit
+import multiprocessing
+
+# https://www.pythontutorial.net/python-concurrency/python-multiprocessing/
 
 
-def raw_to_jpeg(folder, delete_old=False):
-    extensions = ['.CR2', '.ARW', '.NEF']
-    images_processed = 0
+class RawToJpeg():
+    def __init__(self, image_folder, delete_old=False, replace=False) -> None:
+        self.delete_old = delete_old
+        self.replace = replace
 
-    for directory, subdirectory, files in os.walk(folder):
-        for file in files:
+        if self.delete_old:
+            print('\nWARNING: the program is currently configured to delete old images. Are you sure?\n')
+            confirm = input('Are you sure? (y/n):')
+            if confirm != 'y':
+                exit('\Conversion cancelled.\n')
+    
+        self.image_folder = image_folder
 
-            photo_path = os.path.join(directory, file)
+        self.extensions = ('.CR2', '.ARW', '.NEF', '.nef', '.cr2', '.arw', '.DNG', '.dng')
 
-            for extension in extensions:
-                if photo_path.endswith(extension):
+        processes = []
 
-                    converted_path = f'{os.path.splitext(photo_path)[0]}.jpg'
-                    if not os.path.exists(converted_path):
-                        with rawpy.imread(photo_path) as raw:
-                            rgb = raw.postprocess(
-                                gamma=(1.25, 4.5),
-                                bright=1.1,
-                                no_auto_bright=False,
-                                use_camera_wb=True
-                            )
+        images_processed = 0
 
-                        imageio.imsave(converted_path, rgb)
-                        print('Converted image:', converted_path)
-                        images_processed += 1
+        for directory, _, files in walk(self.image_folder):
+            for file in files:
+                if file.endswith(self.extensions):
+                    filepath = path.join(directory, file)
+                    processes.append(multiprocessing.Process(target=self.convert_image, args=[filepath]))
+        
+        print(f'Added {len(processes)} images for conversion.')
 
-                        if delete_old:
-                            os.remove(photo_path)
-                            print('Deleted file:', photo_path)
+        print('Starting conversion...')
+        
+        for process in processes:
+            process.start()
 
-                    else:
-                        print('Image already exported:', converted_path)
+        for process in processes:
+            process.join()
+            images_processed += 1
 
-    print('Export Complete. Images processed:', images_processed)
+        print(f'Conversion complete. {images_processed} images processed')
 
+    def convert_image(self, photo_path):
+        converted_path = f'{path.splitext(photo_path)[0]}.jpg'
+        if not path.exists(converted_path) or self.replace:
+            try:
+                with rawpy.imread(photo_path) as raw:
+                    rgb = raw.postprocess(
+                        gamma=(1.25, 4.5),
+                        bright=1.1,
+                        no_auto_bright=False,
+                        use_camera_wb=True
+                    )
+
+                imageio.imsave(converted_path, rgb)
+                print(f'Exported image: {converted_path}')
+
+            except Exception as e:
+                print(f'Error loading file {photo_path}')
+
+            if self.delete_old:
+                # print(f'Deleted file: {photo_path}')
+                remove(photo_path)
 
 if __name__ == '__main__':
-    # put the starting directory here:
-    photos_folder = 'F:\PHOTOS'
-    raw_to_jpeg(photos_folder)
+    RawToJpeg(image_folder=str(input('\nPhotos Folder: ')), delete_old=True)
